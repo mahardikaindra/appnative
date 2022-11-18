f1c2b5d973c0c6b4b1bc61e60a7d88f247a6b724
rakamahardika <rakamahardika22@gmail.com>
2022-11-18 21:47:49 +0700

    Intial

---
+++ storybook/stories/Container/Container.stories.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import {storiesOf} from '@storybook/react-native';
+import Container from '.';
+
+storiesOf('Container', module).add('to Storybook', () => <Container />);
